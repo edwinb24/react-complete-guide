0274e579e5a11ce13a54103b4d8782c308626a61
edwinb24 <edwin_b24@hotmail.com>
2020-04-10 12:42:58 -0500

    Practicing - HOC 🍜

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,17 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
 
+import Auxiliary from './../../../hoc/Auxiliary'
 import classes from "./Person.css"
 
 class Person extends Component {
     render() {
 
         return (
-        <div className={classes.Person}>
-            <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
-            <p>{this.props.children}</p>
-            <input onChange={this.props.changed} value={this.props.name}></input>
-        </div>
+            <Fragment>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
+                <p>{this.props.children}</p>
+                <input onChange={this.props.changed} value={this.props.name}></input>
+            </Fragment>
         )
     }
 
